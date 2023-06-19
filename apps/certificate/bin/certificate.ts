21f9f547b4edff43aeb57ed6649d8b6778401c0b
Dave Ferguson <dave@harmonydata.co.uk>
2023-06-19 22:40:24 +0100

    GHA - testing

--- apps/certificate/bin/certificate.ts
+++ apps/certificate/bin/certificate.ts
@@ -9,13 +9,6 @@ import { CertificateStack } from '../lib/certificate';
 
 const app = new cdk.App();
 
-console.log ('App')
-console.log (app)
-console.log ('Config get context')
-console.log (app.node.tryGetContext('setup'))
-console.log ('Dev get context')
-console.log (app.node.tryGetContext('env'))
-
 const ensureString: (object: { [name: string]: any }, propName: string)=>string = (object, propName) => {
   if (!object[propName] || object[propName].trim().length === 0){
     throw new Error(propName + ' does not exist or is empty')
@@ -25,19 +18,7 @@ const ensureString: (object: { [name: string]: any }, propName: string)=>string 
 
 const getConfig = () => {
 
-  // Check the config parameter is set
-  let setup = app.node.tryGetContext('setup')
-  let env = app.node.tryGetContext('env')
-  console.log ('Env')
-  console.log (env)
-  // 
-  if (( !setup && !env ) || (setup && ['dev', 'stg', 'prd'].includes(env))){
-    throw new Error("Need to pass in either `-c setup=true` or `-c env=dev|stg|prd`")
-    process.exit(1)
-  } 
-
-  let unparsedEnv = yaml.load(fs.readFileSync(path.resolve("./config/"+(!env?'setup':env)+'.yaml'), "utf8"))
-  console.log (JSON.stringify(unparsedEnv))
+  let unparsedEnv = yaml.load(fs.readFileSync(path.resolve("./config/setup.yaml"), "utf8"))
 
   let buildConfig: BuildConfig = {
     App: ensureString(unparsedEnv as object, 'App'),
